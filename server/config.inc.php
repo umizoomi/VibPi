e5df4be101487b765272eff29b855549122c1776
Umizoomi <github@team-umizoomi.nl>
2014-02-20 10:54:14 +0100

    Minor changes to the config file

--- server/config.inc.php
+++ server/config.inc.php
@@ -5,17 +5,13 @@ DEFINE ('DBPW', '');
 DEFINE ('DBHOST', '');
 DEFINE ('DBNAME', '');
 
-if ($dbc = mysql_connect(DBHOST, DBUSER, DBPW)) {
+if ($dbc = mysql_connect(DBHOST, DBUSER, DBPW)){
     if (!mysql_select_db(DBNAME)) {
-        trigger_error("Can't find database, y u no fix? <br />MySQL Error: " . mysql_error());
+        trigger_error("Can't find database '".$DBNAME."', y u no fix? <br />MySQL Error: " . mysql_error());
         exit();
-    } else{
-        //echo "connection works";
     }
-} else {
-    echo "swag";
+} else{
     trigger_error("Can't connect to MySQL!<br />MySQL Error: " . mysql_error());
     exit();
 }
-
 ?>
