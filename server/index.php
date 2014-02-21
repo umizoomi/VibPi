21d2cbed68ed71c89786cffe726123b1182015ce
Umizoomi <github@team-umizoomi.nl>
2014-02-21 13:22:30 +0100

    Updated index: now showing all events occurred.

--- server/index.php
+++ server/index.php
@@ -1,6 +1,11 @@
 <?php
 include('db.php');
 
-print_r("Jabba the wat"));
+$events = getAllEvents();
 
+for($i = 0; $i <= count($events)-1; $i++){
+    $starttime = explode(" ", $events[$i]['starttime']);
+    $endtime = explode(" ", $events[$i]['endtime']);
+    echo '<a href="event.php?event='.$events[$i]['id'].'">Event: '.$events[$i]['id'].' Device: '.$events[$i]['device'].'. '.$starttime[0].' '.$starttime[1].' - '.$endtime[1].'</a><br>';
+}
 ?>
