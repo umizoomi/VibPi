39141aaeced9d79591dc265db7b53f101f56cfa9
Umizoomi <jamilio@rasidi.nl>
2014-05-19 11:32:15 +0200

    updated noob spacing

--- server/post/index.php
+++ server/post/index.php
@@ -1,39 +1,40 @@
 <?php
+include('../db.php');
 
-    include('../db.php');
-    
-    $data = file_get_contents("php://input");
-    $json = json_decode($data, true);
-    $device;
-    $starttime;
-    $endtime;
-    $eventid = getNextEventID();
-    $measurequery;
-    $amount = count($json);
-	
-	foreach($json as $key => $value){
-		if($key == "device"){
-			$device = $value;
-		} else if($key == "starttime"){
-		    $starttime = $value;
-		} else if($key == "endtime"){
-		    $endtime = $value;
-		} else{
-		    $measurequery .= "('--','".$value['ts']."','".$value['ms']."','".$value['x']."','".$value['y']."','".$value['z']."'), ";
-		}
-	}
-    $device = getDeviceFromKey($device);
-    $eventquery = "('".$device['id']."','".$starttime."','".$endtime."')";
-    if (addEvent($eventquery) != true){
-        header('Fuck I have an error.', true, 500);
-    }
-    
-    $measurequery = str_replace('--', $eventid, $measurequery);
-    $measurequery = chop($measurequery, ", ");
-    
-    if(addMeasure($measurequery) != true){
-        header('Fuck I have an error.', true, 500);
+$data = file_get_contents("php://input");
+$json = json_decode($data, true);
+$device;
+$starttime;
+$endtime;
+$eventid = getNextEventID();
+$measurequery;
+$amount = count($json);
+
+foreach($json as $key => $value){
+    if($key == "device"){
+        $device = $value;
+    } else if($key == "starttime"){
+        $starttime = $value;
+    } else if($key == "endtime"){
+        $endtime = $value;
     } else{
-        header(':', true, 200);
+        $measurequery .= "('--','".$value['ts']."','".$value['ms']."','".$value['x']."','".$value['y']."','".$value['z']."'), ";
     }
-?>
+}
+
+$device = getDeviceFromKey($device);
+$eventquery = "('".$device['id']."','".$starttime."','".$endtime."')";
+
+if (addEvent($eventquery) != true){
+    header('Fuck I have an error.', true, 500);
+    exit;
+}
+
+$measurequery = str_replace('--', $eventid, $measurequery);
+$measurequery = chop($measurequery, ", ");
+
+if(addMeasure($measurequery) != true){
+    header('Fuck I have an error.', true, 500);
+} else{
+    header(':', true, 200);
+}
